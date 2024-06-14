918f2f5d0320877ad64ac6ba4d02669d3db5b8e9
Tamara Rahimi <mahra.rah@gmail.com>
2024-06-14 09:56:38 +0200

    Simplify to use auto instrumentation

--- src/function/durable_function.py
+++ src/function/durable_function.py
@@ -19,56 +19,36 @@ tracer = trace.get_tracer(__name__)
 @bp.route(route="handlers")
 @bp.durable_client_input(client_name="client")
 async def start_orchestrator(req: func.HttpRequest, client, context):
-    carrier = {
-        "traceparent": context.trace_context.Traceparent,
-        "tracestate": context.trace_context.Tracestate,
-    }
 
-    # This manual trace context is needed to correlate the host logs with the ones from the worker
-    with tracer.start_as_current_span(
-        "start_orchestrator", context=extract(carrier)
-    ) as span:
+    logger.info("Starting new orchestration client")
+    job_id = str(uuid.uuid4())
+    instance_id = await client.start_new("my_orchestrator")
 
-        logger.info("Starting new orchestration client")
-        job_id = str(uuid.uuid4())
-        child_span = _extract_context(span)
-        instance_id = await client.start_new(
-            "my_orchestrator", instance_id=job_id, client_input=child_span
-        )
-
-        logging.info(f"Started orchestration with ID = '{instance_id}'.")
-        return client.create_check_status_response(req, instance_id)
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+    return client.create_check_status_response(req, instance_id)
 
 
 @bp.orchestration_trigger(context_name="context")
 def my_orchestrator(context: df.DurableOrchestrationContext):
-    ctx = _create_context(context.get_input())
-
-    with tracer.start_as_current_span("my_orchestrator", context=ctx) as span:
-        logger.info("Enter orchestration method")
-        logger.info("Call first action being called")
-        child_span = _extract_context(span)
-        result1 = yield context.call_activity(
-            "say_hello", {"city": "Tokyo", "trace_context": child_span}
-        )
-        logger.info("Call second action being called")
-        result2 = yield context.call_activity(
-            "say_hello", {"city": "Seattle", "trace_context": child_span}
-        )
-        logger.info("Call third action being called")
-        result3 = yield context.call_activity(
-            "say_hello", {"city": "London", "trace_context": child_span}
-        )
-        logger.info("Finish orchestration method")
-        return [result1, result2, result3]
+    logger.info("Enter orchestration method")
+    logger.info("Call first action being called")
+    result1 = yield context.call_activity(
+        "say_hello",
+        "Tokyo",
+    )
+    logger.info("Call second action being called")
+    result2 = yield context.call_activity("say_hello", "Seattle")
+    logger.info("Call third action being called")
+    result3 = yield context.call_activity("say_hello", "London")
+    logger.info("Finish orchestration method")
+    return [result1, result2, result3]
 
 
-@bp.activity_trigger(input_name="body")
-def say_hello(body: dict, context: func.Context) -> str:
-    ctx = _create_context(body["trace_context"])
-    with tracer.start_as_current_span("say_hello", context=ctx):
-        logger.info("Enter activity method")
-        return f"Hello {body['city']}!"
+@bp.activity_trigger(input_name="city")
+def say_hello(city: dict, context: func.Context) -> str:
+
+    logger.info("Enter activity method")
+    return f"Hello {city}!"
 
 
 def _extract_context(span: Span):
--- src/function/function_app.py
+++ src/function/function_app.py
@@ -1,40 +1,11 @@
-import logging
 import os
 
 import azure.functions as func
+from azure.monitor.opentelemetry import configure_azure_monitor
 from durable_function import bp
 from health import health_bp
-from observability_exporters import setup_telemetry_export
-from opentelemetry.instrumentation.aiohttp_client import \
-    AioHttpClientInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.instrumentation.urllib3 import URLLib3Instrumentor
 
-root_logger = logging.getLogger()
-for handler in root_logger.handlers[:]:
-    root_logger.removeHandler(handler)
-
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.INFO)
-
-logging.getLogger("urllib3").setLevel(logging.INFO)
-logging.getLogger("azure").setLevel(logging.WARNING)
-
-AioHttpClientInstrumentor().instrument()
-RequestsInstrumentor().instrument()
-URLLib3Instrumentor().instrument()
-
-
-setup_telemetry_export(
-    service_name=os.getenv("WEBSITE_SITE_NAME", "orchestration-function"),
-    service_instance_id=os.getenv("WEBSITE_INSTANCE_ID", "instance-1"),
-    service_version="1.0.0",
-    application_insights_connection_string=os.getenv(
-        "APPLICATIONINSIGHTS_CONNECTION_STRING", None
-    ),
-    logger=root_logger,
-)
-
+configure_azure_monitor()
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 app.register_functions(bp)
