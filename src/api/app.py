15b8bc320f65fe2a1b290f7276a4df42cb31f7d7
Jean-Baptiste Ranson <jeanbaptiste.ranson@non.se.com>
2024-06-19 08:51:04 +0200

    Introduce queue consumer

--- src/api/app.py
+++ src/api/app.py
@@ -1,5 +1,8 @@
 import logging
 import os
+import asyncio
+import json
+import base64
 
 import handler
 from fastapi import FastAPI
@@ -10,7 +13,17 @@ from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.instrumentation.urllib3 import URLLib3Instrumentor
 
+from opentelemetry import trace
+from opentelemetry.trace import SpanContext
+
+from azure.storage.queue import QueueClient
+from typing import Any, Set
+
 app = FastAPI()
+
+background_tasks: Set[asyncio.Task[Any]] = set()
+
+tracer = trace.get_tracer(__name__)
 
 root_logger = logging.getLogger()
 for handler in root_logger.handlers[:]:
@@ -35,3 +48,48 @@ FastAPIInstrumentor.instrument_app(app)
 AioHttpClientInstrumentor().instrument()
 RequestsInstrumentor().instrument()
 URLLib3Instrumentor().instrument()
+
+logger = logging.getLogger()
+
+@app.on_event("startup")
+async def start_up():
+    logger.info("FastAPI starting")
+    queue_client = QueueClient.from_connection_string("AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;", "spike-api-events")
+    start_event_queue_consumer(queue_client)
+
+def start_event_queue_consumer(queue_client: QueueClient) -> None:
+    loop = asyncio.get_event_loop()
+    consome_task = loop.create_task(consume_messages(queue_client))
+    background_tasks.add(consome_task)
+
+async def consume_messages(queue_client: QueueClient) -> None:
+    while True:
+        await process_messages(queue_client=queue_client)
+
+async def process_messages(queue_client: QueueClient) -> None:
+    messages = queue_client.receive_messages(visibility_timeout=30)
+    no_message_received = True
+    for message in messages:
+        no_message_received = False
+        
+        content = base64.b64decode(message.content).decode('utf-8')
+        print(content)
+        logger.info(f"Processing message: {content}")
+
+        ctx = _create_context(json.loads(content))
+        with tracer.start_as_current_span("api_service", context=ctx):
+            logger.info("Enter api_service_message_consumer")
+
+        queue_client.delete_message(message=message.id, pop_receipt=message.pop_receipt)
+
+    if no_message_received:
+        await asyncio.sleep(1.0)
+
+def _create_context(dict):
+    span_ctx = SpanContext(
+        trace_id=dict["trace_id"],
+        span_id=dict["span_id"],
+        is_remote=True
+    )
+    return trace.set_span_in_context(trace.NonRecordingSpan(span_ctx))
+
--- src/function/durable_function.py
+++ src/function/durable_function.py
@@ -1,9 +1,13 @@
 import logging
+import uuid
+import json
 
 import azure.durable_functions as df
 import azure.functions as func
+
 from opentelemetry import trace
 from opentelemetry.propagate import extract
+from opentelemetry.trace import Span, SpanContext
 
 # To learn more about blueprints in the Python prog model V2,
 # see: https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference-python?tabs=asgi%2Capplication-level&pivots=python-mode-decorators#blueprints
@@ -24,11 +28,15 @@ async def start_orchestrator(req: func.HttpRequest, client, context):
 
     # This manual trace context is needed to correlate the host logs with the ones from the worker
     with tracer.start_as_current_span(
-        "start_orchestrator",
-        context=extract(carrier),
-    ):
+        "start_orchestrator", context=extract(carrier)
+    ) as span:
+
         logger.info("Starting new orchestration client")
-        instance_id = await client.start_new("my_orchestrator")
+        job_id = str(uuid.uuid4())
+        child_span = _extract_context(span)
+        instance_id = await client.start_new(
+            "my_orchestrator", instance_id=job_id, client_input=child_span
+        )
 
         logging.info(f"Started orchestration with ID = '{instance_id}'.")
         return client.create_check_status_response(req, instance_id)
@@ -36,18 +44,94 @@ async def start_orchestrator(req: func.HttpRequest, client, context):
 
 @bp.orchestration_trigger(context_name="context")
 def my_orchestrator(context: df.DurableOrchestrationContext):
-    logger.info("Enter orchestration method")
-    logger.info("Call first action being called")
-    result1 = yield context.call_activity("say_hello", "Tokyo")
-    logger.info("Call second action being called")
-    result2 = yield context.call_activity("say_hello", "Seattle")
-    logger.info("Call third action being called")
-    result3 = yield context.call_activity("say_hello", "London")
-    logger.info("Finish orchestration method")
-    return [result1, result2, result3]
+    ctx = _create_context(context.get_input())
+
+    with tracer.start_as_current_span("my_orchestrator", context=ctx) as span:
+        logger.info("Enter orchestration method")
+
+        logger.info("Call first action being called")
+        child_span = _extract_context(span)
+        result1 = yield context.call_activity(
+            "say_hello", {"city": "Tokyo", "trace_context": child_span}
+        )
+        logger.info("Call second action being called")
+        result2 = yield context.call_activity(
+            "say_hello", {"city": "Seattle", "trace_context": child_span}
+        )
+        logger.info("Call third action being called")
+        result3 = yield context.call_activity(
+            "say_hello", {"city": "London", "trace_context": child_span}
+        )
+        logger.info("Finish orchestration method")
+
+        logger.info("Persist entity state")
+        entityId = df.EntityId("main_entity", 1)
+        yield context.call_entity(entityId, "set", child_span)
+
+        return [result1, result2, result3]
 
 
-@bp.activity_trigger(input_name="city")
-def say_hello(city: str) -> str:
-    logger.info("Enter activity method")
-    return f"Hello {city}!"
+@bp.activity_trigger(input_name="body")
+def say_hello(body: dict, context: func.Context) -> str:
+    ctx = _create_context(body["trace_context"])
+    with tracer.start_as_current_span("say_hello", context=ctx):
+        logger.info("Enter activity method")
+        return f"Hello {body['city']}!"
+
+
+@bp.entity_trigger(context_name="context", entity_name="main_entity")
+def persist_entity_state(context: df.DurableEntityContext) -> None:
+    ctx = _create_context(context.get_input())
+    with tracer.start_as_current_span("set_entity", context=ctx):
+        logger.info("persist_entity_state being called")
+        operation = context.operation_name
+        if operation == "set":
+            context.set_state(context.get_input())
+
+
+@bp.queue_trigger(
+    arg_name="amlEventQueueMessage",
+    queue_name="spike-aml-events",
+    connection="AML_EVENTS_QUEUE_CONNECTION",
+)
+@bp.queue_output(
+    arg_name="apiEventOutputBinding",
+    queue_name="spike-api-events",
+    connection="API_EVENTS_QUEUE_CONNECTION",
+)
+@bp.durable_client_input(client_name="client")
+async def process_aml_event(
+    amlEventQueueMessage: func.QueueMessage,
+    client: df.DurableOrchestrationClient,
+    apiEventOutputBinding: func.Out[str],
+    context,
+) -> None:
+    
+    carrier = {
+        "traceparent": context.trace_context.Traceparent,
+        "tracestate": context.trace_context.Tracestate,
+    }
+
+    with tracer.start_as_current_span("queue_trigger", context=extract(carrier)) as span:
+        
+        logger.info("About to process inbound queue message")
+
+        child_span = _extract_context(span)
+        apiEventOutputBinding.set(json.dumps(child_span))
+
+
+def _extract_context(span: Span):
+    ctx = span.get_span_context()
+    return {
+        "trace_id": ctx.trace_id,
+        "span_id": ctx.span_id,
+    }
+
+
+def _create_context(span_context):
+    span_ctx = SpanContext(
+        trace_id=span_context["trace_id"],
+        span_id=span_context["span_id"],
+        is_remote=True,
+    )
+    return trace.set_span_in_context(trace.NonRecordingSpan(span_ctx))
